822ca7db1ce887442833f65f3d9ee8ce0c1e19fa
jinlingxing <jinlingxing115@gmail.com>
2022-10-03 20:49:39 -0400

    array/minimum_time_to_make_rope_colorful

---
+++ array/1578.minimum_time_to_make_rope_colorful.py
@@ -0,0 +1,32 @@
+"""
+Alice has n balloons arranged on a rope. You are given a 0-indexed string colors where colors[i] is the color of the ith balloon.
+
+Alice wants the rope to be colorful. She does not want two consecutive balloons to be of the same color, so she asks Bob for help. Bob can remove some balloons from the rope to make it colorful. You are given a 0-indexed integer array neededTime where neededTime[i] is the time (in seconds) that Bob needs to remove the ith balloon from the rope.
+
+Return the minimum time Bob needs to make the rope colorful.
+
+Input: colors = "abaac", neededTime = [1,2,3,4,5]
+Output: 3
+Explanation: In the above image, 'a' is blue, 'b' is red, and 'c' is green.
+Bob can remove the blue balloon at index 2. This takes 3 seconds.
+There are no longer two consecutive balloons of the same color. Total time = 3.
+
+
+Input: colors = "aabaa", neededTime = [1,2,3,4,1]
+Output: 2
+Explanation: Bob will remove the ballons at indices 0 and 4. Each ballon takes 1 second to remove.
+There are no longer two consecutive balloons of the same color. Total time = 1 + 1 = 2.
+"""
+
+
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        res = 0
+        temp_max = 0
+        for i in range(len(colors)):
+            if i>0 and colors[i] == colors[i-1]:
+                res += min(neededTime[i],temp_max)
+                temp_max = max(temp_max,neededTime[i])
+            else:
+                temp_max = neededTime[i]
+        return res
