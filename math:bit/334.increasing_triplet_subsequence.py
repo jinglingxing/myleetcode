acc568cf8ec63401c7b086ac23e3c2460ff08c10
jinlingxing <jinlingxing115@gmail.com>
2022-10-10 21:10:49 -0400

    python/334.increasing_triplet_subsequence

---
+++ math:bit/334.increasing_triplet_subsequence.py
@@ -0,0 +1,27 @@
+"""
+Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+"""
+
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first, second = float('inf'), float('inf')
+        for num in nums:
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            else:
+                return True
+        return False
+
+
+
