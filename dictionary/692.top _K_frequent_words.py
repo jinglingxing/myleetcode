d11263d5840d0a39aec9044054b6d65f11251e9c
jinlingxing <jinlingxing115@gmail.com>
2022-10-18 21:54:57 -0400

    dictionary_sort/top_k_frequent_words

---
+++ dictionary/692.top _K_frequent_words.py
@@ -0,0 +1,22 @@
+"""
+Given an array of strings words and an integer k, return the k most frequent strings.
+
+Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.
+
+Input: ["i","love","leetcode","i","love","coding"], k=3
+Output: ["i","love","coding"]
+"""
+
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        dic = {}
+        for word in words:
+            if word not in dic:
+                dic[word] = 1
+            else:
+                dic[word] += 1
+
+        sorted_dic = {key: value for key, value in sorted(dic.items(), key=lambda item: (-item[1], item[0]))}
+
+        return list(sorted_dic.keys())[:k]
