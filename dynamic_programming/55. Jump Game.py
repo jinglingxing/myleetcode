b0462b70dba3383b1e21a24d12bc915ec07658c6
jinlingxing <jinlingxing115@gmail.com>
2023-06-20 21:35:18 -0400

    55. Jump Game

---
+++ dynamic_programming/55. Jump Game.py
@@ -0,0 +1,22 @@
+"""
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+"""
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        last_good_position = len(nums)-1
+        for i in range(last_good_position, -1, -1):
+            if nums[i]+i >= last_good_position:
+                last_good_position = i
+
+        return last_good_position == 0
