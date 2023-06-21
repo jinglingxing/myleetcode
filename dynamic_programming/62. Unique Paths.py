20fb2f4babcab1c12b12ec9a366009ce2bffbcf8
jinlingxing <jinlingxing115@gmail.com>
2023-06-20 21:36:18 -0400

    62.unique paths

--- dynamic_programming/62. Unique Paths.py
+++ dynamic_programming/62. Unique Paths.py
@@ -27,10 +27,13 @@ Input: m = 7, n = 3
 Output: 28
  
 '''
+
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[1 for i in range(0, n)] for j in range(0, m)]
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
-        return dp[m-1][n-1]
+        dp = [[1 for j in range(m)] for i in range(n)]
+        for i in range(1, n):
+            for j in range(1, m):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        return dp[-1][-1]
