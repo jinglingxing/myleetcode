a65475a8ccfbbb1216a1d67277f20419785deca3
jinlingxing <jinlingxing115@gmail.com>
2023-06-27 17:59:10 -0400

    updated 322

--- dynamic_programming/322. Coin Change.py
+++ dynamic_programming/322. Coin Change.py
@@ -41,11 +41,15 @@ the answer is guaranteed to fit into signed 32-bit integer
 """
 
 class Solution:
-    def change(self, amount: int, coins: List[int]) -> int:
-        dp = [0] * (amount+1)
-        dp[0] = 1
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount+1]*(amount+1)
+        dp[0] = 0
+        print(dp)
         for coin in coins:
             for i in range(coin, amount+1):
-                dp[i] += dp[i-coin]
-        return dp[-1]
+                print(coin, i)
+                dp[i] = min(dp[i], dp[i-coin]+1)
+
+
+        return -1
             
