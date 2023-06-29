74458524981d5e7859b000cbe3f3a265f01a3109
jinlingxing <jinlingxing115@gmail.com>
2023-06-29 15:25:37 -0400

    516. Longest Palindromic Subsequence

---
+++ palindrome/516. Longest Palindromic Subsequence.py
@@ -0,0 +1,17 @@
+class Solution(object):
+
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1) + 1, len(text2) + 1
+        dp = [[0] * n for i in range(m)]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if text1[i - 1] != text2[j - 1]:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                else:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+
+        return dp[-1][-1]
+
+    def longestPalindromeSubseq(self, s: str) -> int:
+        return self.longestCommonSubsequence(s, s[::-1])
