cd9a4e695577731a61fd741ea6f192263a044509
jinlingxing <jinlingxing115@gmail.com>
2023-06-29 15:27:53 -0400

    131. Palindrome Partitioning

---
+++ palindrome/131. Palindrome Partitioning.py
@@ -0,0 +1,16 @@
+class Solution:
+    @lru_cache
+    def partition(self, s: str) -> List[List[str]]:
+        if len(s) == 0:
+            return [[]]
+
+        ans = []
+        for i in range(1, len(s) + 1):
+            pref = s[:i]
+            if pref == pref[::-1]:
+                suff = s[i:]
+                for j in self.partition(suff):
+                    ans.append([s[:i]] + j)
+
+        return ans
+
