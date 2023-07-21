25ea6cdfc527e03308eba3c593aef59df30978c2
jinlingxing <jinlingxing115@gmail.com>
2023-07-21 17:47:33 -0400

    295. Find Median from Data Stream

---
+++ class/295. Find Median from Data Stream.py
@@ -0,0 +1,53 @@
+"""
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+
+
+Example 1:
+
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+
+"""
+
+
+class MedianFinder:
+
+    def __init__(self):
+        self.nums = []
+
+    def addNum(self, num: int) -> None:
+        self.nums.append(num)
+
+    def findMedian(self) -> float:
+        self.nums.sort()
+        if len(self.nums) % 2 == 1:
+            # odd numbers
+            return self.nums[len(self.nums) // 2]
+        else:
+            # even numbers
+            idx = len(self.nums) // 2
+            return (self.nums[idx] + self.nums[idx - 1]) / 2
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
