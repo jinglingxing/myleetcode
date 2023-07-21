3fddf86aadc806638a228ed936f0475c034561e4
jinlingxing <jinlingxing115@gmail.com>
2023-07-21 15:13:17 -0400

    dict/12. Integer to Roman

---
+++ dictionary/12. Integer to Roman.py
@@ -0,0 +1,16 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        dic = {
+            1: 'I', 4: 'IV', 5: 'V',
+            9: 'IX', 10: 'X', 40: 'XL',
+            50: 'L', 90: 'XC', 100: 'C',
+            400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'
+        }
+
+        res = ''
+        for key in sorted(dic.keys(), reverse=True):
+            while num - key >= 0:
+                num -= key
+                res += dic[key]
+
+        return res
