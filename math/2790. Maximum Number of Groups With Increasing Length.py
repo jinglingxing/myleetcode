13166c4cc7a1c2f6d26fa797e488473a805fe424
jinlingxing <jinlingxing115@gmail.com>
2023-07-23 12:48:50 -0400

    2790. Maximum Number of Groups With Increasing Length

---
+++ math/2790. Maximum Number of Groups With Increasing Length.py
@@ -0,0 +1,38 @@
+"""
+You are given a 0-indexed array usageLimits of length n.
+
+Your task is to create groups using numbers from 0 to n - 1, ensuring that each number, i, is used no more than usageLimits[i] times in total across all groups. You must also satisfy the following conditions:
+
+Each group must consist of distinct numbers, meaning that no duplicate numbers are allowed within a single group.
+Each group (except the first one) must have a length strictly greater than the previous group.
+Return an integer denoting the maximum number of groups you can create while satisfying these conditions.
+
+
+Input: usageLimits = [1,2,5]
+Output: 3
+Explanation: In this example, we can use 0 at most once, 1 at most twice, and 2 at most five times.
+One way of creating the maximum number of groups while satisfying the conditions is:
+Group 1 contains the number [2].
+Group 2 contains the numbers [1,2].
+Group 3 contains the numbers [0,1,2].
+It can be shown that the maximum number of groups is 3.
+So, the output is 3.
+
+"""
+
+
+class Solution:
+    def maxIncreasingGroups(self, usageLimits: List[int]) -> int:
+        # math
+        usageLimits.sort()
+
+        total, count = 0, 0
+        # the group will never be more than len(usageLimits)
+        for i in range(len(usageLimits)):
+            total += usageLimits[i]
+
+            if total >= ((count + 1) * (count + 2)) // 2:
+                count += 1
+
+        return count
+
