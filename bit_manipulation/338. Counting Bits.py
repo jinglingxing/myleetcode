291fd525cb03b0b5d2a6389a42d9e045cd825d2e
jinlingxing <jinlingxing115@gmail.com>
2023-07-24 16:11:30 -0400

    bit/338.counting bits

--- bit_manipulation/338. Counting Bits.py
+++ bit_manipulation/338. Counting Bits.py
@@ -5,6 +5,27 @@ Created on Sun Feb  9 22:45:01 2020
 
 @author: jinlingxing
 """
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        # n = 5
+        res = [0] * (n + 1)
+
+        for i in range(0, n + 1):
+            print(i, i >> 1, i % 2)
+            # i=0, i>>1=0, i%2=0, res[0]=0
+            # i=1, i>>1=0, i%2=1, res[1]=1
+            # i=2, 2>>1=1, i%2=0, res[2]=res[1]+0=1
+            # i=3, 3>>1=1, i%2=1, res[3]=res[2]+1=2
+            # i=4, 4>>1=2, i%2=0, res[4]=res[2]+0=1
+            # i=5, 5>>1=2, i%2=1, res[5]=res[2]+1=2
+            # res = [0,1,1,2,1,2]
+            res[i] = res[i >> 1] + i % 2
+        return res
+
+
+
 class Solution(object):
 
     def countBits2(self, num):
@@ -13,12 +34,8 @@ class Solution(object):
         :rtype: List[int]
         """
         res = [0] * (num + 1)
-        for i in range(0,num+1):
-            res[i]= res[i//2] + i%2
+        for i in range(0, num+1):
+            res[i] = res[i//2] + i % 2
         return res
 
 
-if __name__ == "__main__":
-    sol = Solution()
-    numb = 5
-    print(sol.countBits2(numb))
